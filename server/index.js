4b8e439ad841f83db965c747a37da312b03c5b80
Qamar Ul ISlam <qamarislam1232@gmail.com>
2024-03-15 23:13:22 +0500

    index.js file improvements to run

--- server/index.js
+++ server/index.js
@@ -1,29 +1,35 @@
+// packages import
 const express = require('express');
 const app = express();
 const cors = require("cors");
 const mongoose = require('mongoose');
 const { notFound, errorHandler } = require('./middlewares/ErrorHandler');
 const recipeRoutes = require('./routes/recipes');
-const PORT = process.env.PORT || 45200;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cheff_connect_database'; 
 
+// From environment variables
+const PORT = process.env.PORT || 45200;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cheff_connect_database';
+
+// CORS JSON and URL encoded middlewares enabled
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Routers
 app.use('/api/recipes', recipeRoutes);
 
+// Error middlewares
 app.use(notFound);
 app.use(errorHandler);
 
-
+// Database connection
 main().catch(err => console.log(err))
 async function main() {
   await mongoose.connect(MONGODB_URI);
   console.log("connected");
 }
 
-//server
+// Server
 app.listen(PORT, () => {
   console.log(`APP IS LISTENING ON PORT ${PORT}`);
 })

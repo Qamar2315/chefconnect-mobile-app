4daa3631f2f889da7f743b4e2a616229fdde4f86
Qamar Ul ISlam <qamarislam1232@gmail.com>
2024-03-15 23:02:25 +0500

    imported environment variables

--- server/index.js
+++ server/index.js
@@ -1,26 +1,29 @@
-const express= require('express');
-const app= express();
-const cors= require("cors");
-const mongoose=require('mongoose');
-const {notFound, errorHandler}= require('./middlewares/ErrorHandler');
-const recipeRoutes= require('./routes/recipes');
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const mongoose = require('mongoose');
+const { notFound, errorHandler } = require('./middlewares/ErrorHandler');
+const recipeRoutes = require('./routes/recipes');
+const PORT = process.env.PORT || 45200;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cheff_connect_database'; 
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/recipes',recipeRoutes);
+app.use('/api/recipes', recipeRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
 
+
 main().catch(err => console.log(err))
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/cheff_connect_database');
+  await mongoose.connect(MONGODB_URI);
   console.log("connected");
 }
 
 //server
-app.listen(9090,()=>{
-    console.log("APP IS LISTENING ON PORT 9090");
+app.listen(PORT, () => {
+  console.log(`APP IS LISTENING ON PORT ${PORT}`);
 })

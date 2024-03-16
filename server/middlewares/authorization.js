71b4aafc48dde250eae0fa7ae63b017301e1a788
Qamar Ul ISlam <qamarislam1232@gmail.com>
2024-03-17 02:20:56 +0500

    added isRecipeAuthor authorization in authorization middleware

--- server/middlewares/authorization.js
+++ server/middlewares/authorization.js
@@ -1,14 +1,31 @@
 const asyncHandler = require('../utilities/CatchAsync');
 const AppError = require('../utilities/AppError');
+const Recipe = require('../model/recipe');
 
 const isProfileAuthor = asyncHandler(async (req, res, next) => {
     const { userId } = req.params;
     if (userId !== req.user._id) {
         throw new AppError("Not Authorized", 401);
     }
-next();
+    next();
+});
+
+const isRecipeAuthor = asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    const recipe = await Recipe.findById(id).populate('author');
+    if (!recipe) {
+        throw new AppError("Recipe Not Found", 404);
+    } else {
+        const authorId = recipe.author._id.toString();
+        if (authorId !== req.user._id) {
+            throw new AppError("Not Authorized", 401);
+        } else {
+            next();
+        }
+    }
 });
 
 module.exports = {
-    isProfileAuthor
+    isProfileAuthor,
+    isRecipeAuthor
 }

275f32c33c729527c811c8864d643fe410719cd0
Qamar Ul ISlam <qamarislam1232@gmail.com>
2024-03-20 19:55:13 +0500

    Created navigation stacks

---
+++ client/src/navigation/AppStack.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import AddRecipeScreen from '../screens/AddRecipeScreen';
+import ViewRecipeScreen from '../screens/ViewRecipeScreen';
+import ViewProfileScreen from '../screens/ViewProfileScreen';
+import ChangePasswordScreen from '../screens/ChangePasswordScreen';
+import UpdateProfileScreen from '../screens/UpdateProfileScreen';
+import UpdateRecipeScreen from '../screens/UpdateRecipeScreen';
+import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+const AppStack = () => {
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name='home' component={HomeScreen} />
+            <Stack.Screen name='add-recipe' component={AddRecipeScreen} />
+            <Stack.Screen name='view-recipe' component={ViewRecipeScreen} />
+            <Stack.Screen name='view-profile' component={ViewProfileScreen} />
+            <Stack.Screen name='change-password' component={ChangePasswordScreen} />
+            <Stack.Screen name='update-profile' component={UpdateProfileScreen} />
+            <Stack.Screen name='update-recipe' component={UpdateRecipeScreen} />
+        </Stack.Navigator>
+    );
+};
+
+export default AppStack;

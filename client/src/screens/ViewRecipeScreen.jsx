cbf4485eee99495e102e06c7882e7333bb1bc513
Qamar Ul ISlam <qamarislam1232@gmail.com>
2024-03-24 00:51:11 +0500

    finish work on view recipe screen including interaction with the backend

--- client/src/screens/ViewRecipeScreen.jsx
+++ client/src/screens/ViewRecipeScreen.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, ScrollView, TouchableOpacity, TextInput } from 'react-native';
+import { View, Text, ScrollView, TouchableOpacity, TextInput, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useState, useContext, useEffect } from 'react';
 import { useRoute } from '@react-navigation/native';
@@ -8,40 +8,72 @@ import axios from 'axios';
 import { BASE_URL } from '../../config';
 
 const ViewRecipeScreen = () => {
-
     const route = useRoute();
     const { recipeId } = route.params;
     const navigation = useNavigation();
     const { userSession } = useContext(AuthContext);
     const [recipe, setRecipe] = useState(null);
 
-    const [rating, setRating] = useState(0);
+    const [rating, setRating] = useState('0');
     const [description, setDescription] = useState('');
 
-    const handleDeleteReview = (index) => {
-        // Create a copy of the current reviews array
-        const updatedReviews = [...recipe.reviews];
-        // Remove the review at the specified index
-        updatedReviews.splice(index, 1);
-        // Update the recipe state with the updated reviews array
-        setRecipe({ ...recipe, reviews: updatedReviews });
-        // You can send an API request to delete the review from the backend as well
+    const handleDeleteReview = async (index) => {
+        try {
+            // Get the ID of the review to be deleted based on its index in the reviews array
+            const reviewIdToDelete = recipe.reviews[index]._id;
+
+            // Send a DELETE request to your backend API to delete the review
+            const response = await axios.delete(
+                `${BASE_URL}/api/recipes/${recipeId}/reviews/${reviewIdToDelete}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${userSession.token}`, // Assuming userSession.token contains the JWT token
+                    },
+                }
+            );
+            // console.log('Add Review Response:', response.data);
+            if (response.data.success) {
+                // Create a copy of the current reviews array
+                const updatedReviews = [...recipe.reviews];
+                // Remove the review at the specified index
+                updatedReviews.splice(index, 1);
+                // Update the recipe state with the updated reviews array
+                setRecipe({ ...recipe, reviews: updatedReviews });
+                Alert.alert("Congrats", response.data.message)
+            } else {
+                Alert.alert("Error", response.data.message)
+            }
+        } catch (error) {
+            console.error('Delete Review Error:', error);
+            // Handle errors such as displaying an error message to the user
+        }
     };
 
     const handleAddReview = async () => {
         // Add the new review to the local state first
-        const newReview = { rating, description, author: 'authorId' }; // Update author ID based on your authentication
+        const newReview = { rating, description, author: { _id: userSession._id, name: userSession.name } }; // Update author ID based on your authentication
         const updatedReviews = [...recipe.reviews, newReview];
         setRecipe({ ...recipe, reviews: updatedReviews });
         try {
-            // Send a POST request to your backend API to add the review
-            const response = await axios.post(`${BASE_URL}/api/recipes/`, {
-                recipeId: recipe._id,
-                rating,
-                description,
-                // Add author ID based on your authentication
-            });
-            console.log('Add Review Response:', response.data);
+            const response = await axios.post(
+                `${BASE_URL}/api/recipes/${recipeId}/reviews`,
+                {
+                    rating,
+                    description
+                },
+                {
+                    headers: {
+                        "Content-type": "application/json",
+                        Authorization: `Bearer ${userSession.token}`,
+                    },
+                }
+            );
+            // console.log('Add Review Response:', response.data);
+            if (response.data.success) {
+                Alert.alert("Congrats", response.data.message)
+            } else {
+                Alert.alert("Error", response.data.message)
+            }
             // Reset review fields
             setRating(0);
             setDescription('');
@@ -64,6 +96,48 @@ const ViewRecipeScreen = () => {
             console.error('Error fetching recipe:', error);
             throw error; // Throw the error for handling in the component
         }
+    };
+
+    const handleEditRecipe = () => {
+        navigation.navigate('update-recipe', { recipeId });
+    };
+
+    const handleDeleteRecipe = async () => {
+        // Show an alert to confirm before deleting
+        Alert.alert(
+            'Confirm Deletion',
+            'Are you sure you want to delete this recipe?',
+            [
+                {
+                    text: 'Cancel',
+                    style: 'cancel',
+                },
+                {
+                    text: 'Delete',
+                    style: 'destructive',
+                    onPress: async () => {
+                        try {
+                            // Send a DELETE request to the backend API
+                            const response = await axios.delete(`${BASE_URL}/api/recipes/${recipeId}`, {
+                                headers: {
+                                    Authorization: `Bearer ${userSession.token}`,
+                                },
+                            });
+                            console.log('Delete Recipe Response:', response.data);
+                            if (response.data.success) {
+                                Alert.alert("Congrats", response.data.message);
+                                navigation.navigate('home');
+                            } else {
+                                Alert.alert("Error", response.data.message);
+                            }
+                        } catch (error) {
+                            console.error('Delete Recipe Error:', error);
+                            // Handle errors such as displaying an error message to the user
+                        }
+                    },
+                },
+            ]
+        );
     };
 
     useEffect(() => {
@@ -101,32 +175,46 @@ const ViewRecipeScreen = () => {
             {recipe.tags.map((tag, index) => (
                 <Text key={index} className="mb-1">{tag}</Text>
             ))}
-            <TouchableOpacity
-                className="bg-blue-500 rounded-lg p-2 mt-4"
-                onPress={() => navigation.navigate('Home')} // Navigate to Home screen
-            >
-                <Text className="text-white">Back to Home</Text>
-            </TouchableOpacity>
+            {
+                userSession?._id === recipe.author._id &&
+                <View className="flex-row justify-between px-4 pt-4">
+                    <TouchableOpacity
+                        className="bg-blue-500 rounded-lg p-2"
+                        onPress={handleEditRecipe}
+                    >
+                        <Text className="text-white">Edit Recipe</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        className="bg-red-500 rounded-lg p-2"
+                        onPress={handleDeleteRecipe}
+                    >
+                        <Text className="text-white">Delete Recipe</Text>
+                    </TouchableOpacity>
+                </View>
+            }
             {/* Reviews section */}
-            <View className="mt-4 border-t border-gray-300 pt-4">
-                <Text className="text-xl font-bold mb-2">Reviews:</Text>
-                {recipe.reviews.map((review, index) => (
-                    <View key={index} className="mb-2">
-                        <Text>Rating: {review.rating}</Text>
-                        <Text>Description: {review.description}</Text>
-                        <Text className="text-sm text-slate-800" >By: {review.author.name}</Text>
-                        {
-                            userSession && userSession._id === review.author._id &&
-                            <TouchableOpacity
-                                className="bg-red-500 rounded-lg p-2 mt-1"
-                                onPress={() => handleDeleteReview(index)}
-                            >
-                                <Text className="text-white">Delete</Text>
-                            </TouchableOpacity>
-                        }
-                    </View>
-                ))}
-            </View>
+            {
+                recipe?.reviews.length > 0 &&
+                <View className="mt-4 border-t border-gray-300 pt-4">
+                    <Text className="text-xl font-bold mb-2">Reviews:</Text>
+                    {recipe.reviews.map((review, index) => (
+                        <View key={index} className="mb-2">
+                            <Text>Rating: {review.rating}</Text>
+                            <Text>Description: {review.description}</Text>
+                            <Text className="text-sm text-slate-800" >By: {review.author.name}</Text>
+                            {
+                                userSession && userSession._id === review.author._id &&
+                                <TouchableOpacity
+                                    className="bg-red-500 rounded-lg p-2 mt-1"
+                                    onPress={() => handleDeleteReview(index)}
+                                >
+                                    <Text className="text-white">Delete</Text>
+                                </TouchableOpacity>
+                            }
+                        </View>
+                    ))}
+                </View>
+            }
             <View className="mt-4 border-t border-gray-300 pt-4 mb-10">
                 <Text className="text-xl font-bold mb-2">Add Review:</Text>
                 <TextInput

862e8320e87e61c8a33c4ed363e7ba141f7a79bc
Qamar Ul ISlam <qamarislam1232@gmail.com>
2024-03-26 00:52:05 +0500

    view profile screen completed along with conection with backend

--- client/src/screens/ViewProfileScreen.jsx
+++ client/src/screens/ViewProfileScreen.jsx
@@ -1,30 +1,93 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, ScrollView, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { recipes } from '../../test'; // Import your recipe data or API call
+import { useRoute } from '@react-navigation/native';
+import { useState, useEffect, useContext } from 'react';
+import LoadingScreen from '../components/LoadingScreen';
+import { AuthContext } from '../helpers/Auth';
+import { BASE_URL } from '../../config';
+import axios from 'axios';
 
 const ProfileScreen = () => {
+    const route = useRoute();
+    const { userSession, isLoading, setIsLoading } = useContext(AuthContext);
     const navigation = useNavigation();
-    const userProfile = {
-        name: 'John Doe',
-        email: 'johndoe@example.com',
-    };
-    const userRecipes = recipes;
+    const { userId } = route.params;
 
-    const handleDeleteRecipe = (recipeId) => {
-        // Implement your delete recipe logic here
-        Alert.alert('Delete Recipe', 'Are you sure you want to delete this recipe?', [
-            { text: 'Cancel', style: 'cancel' },
-            { text: 'Delete', onPress: () => console.log('Delete recipe with ID:', recipeId) },
-        ]);
+    const [userProfile, setUserProfile] = useState(null);
+
+    useEffect(() => {
+        // Function to fetch user data based on userId
+        const fetchUserData = async () => {
+            try {
+                const response = await axios.get(`${BASE_URL}/api/users/${userId}`, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${userSession.token}`, // Include authorization token if needed
+                    },
+                });
+                // Assuming your API returns user data in the response.data
+                setUserProfile(response.data.data);
+                setIsLoading(false);
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+                setIsLoading(false);
+                // Handle error as needed (e.g., show error message to the user)
+            }
+        };
+
+        fetchUserData(); // Call the fetchUserData function when the component mounts
+    }, [userId]); // Fetch data when userId changes
+
+    const handleDeleteRecipe = async (recipeId) => {
+        // Show an alert to confirm before deleting
+        Alert.alert(
+            'Confirm Deletion',
+            'Are you sure you want to delete this recipe?',
+            [
+                {
+                    text: 'Cancel',
+                    style: 'cancel',
+                },
+                {
+                    text: 'Delete',
+                    style: 'destructive',
+                    onPress: async () => {
+                        try {
+                            // Send a DELETE request to the backend API
+                            const response = await axios.delete(`${BASE_URL}/api/recipes/${recipeId}`, {
+                                headers: {
+                                    Authorization: `Bearer ${userSession.token}`,
+                                },
+                            });
+                            console.log('Delete Recipe Response:', response.data);
+                            if (response.data.success) {
+                                Alert.alert("Congrats", response.data.message);
+                                navigation.navigate('home');
+                            } else {
+                                Alert.alert("Error", response.data.message);
+                            }
+                        } catch (error) {
+                            console.error('Delete Recipe Error:', error);
+                            // Handle errors such as displaying an error message to the user
+                        }
+                    },
+                },
+            ]
+        );
     };
 
     const handleViewRecipe = (recipeId) => {
         // Navigate to ViewRecipe screen with the selected recipe ID
-        navigation.navigate('ViewRecipe', { recipeId });
+        navigation.navigate('view-recipe', { recipeId });
     };
 
+    if (isLoading) {
+        return <LoadingScreen />; // You can create a loading spinner or message component
+    }
+
     return (
+        userProfile &&
         <ScrollView className="flex-1 p-4">
             <View className="mt-4">
                 <Text className="text-xl font-bold mb-2">Profile</Text>
@@ -34,7 +97,7 @@ const ProfileScreen = () => {
 
             <TouchableOpacity
                 className="bg-green-500 rounded-lg p-2 mt-4"
-                onPress={() => navigation.navigate('change-password')}
+                onPress={() => navigation.navigate('update-password')}
             >
                 <Text className="text-white">Change Password</Text>
             </TouchableOpacity>
@@ -48,17 +111,20 @@ const ProfileScreen = () => {
 
             <ScrollView className="mt-4">
                 <Text className="text-xl font-bold mb-2">My Recipes</Text>
-                {userRecipes.map(recipe => (
+                {userProfile.recipes.map(recipe => (
                     <View key={recipe._id} className="mb-4">
-                        <Text className="text-lg font-bold">{recipe.title}</Text>
-                        <Text className="text-sm">{recipe.description}</Text>
-                        <Text className="text-sm font-thin">{recipe.author}</Text>
-
-                        <TouchableOpacity
-                            className="bg-red-500 rounded-sm p-2 mt-1"
-                            onPress={() => handleDeleteRecipe(recipe._id)}
-                        >
-                            <Text className="text-white">Delete Recipe</Text>
+                        <TouchableOpacity onPress={() => { handleViewRecipe(recipe._id) }}>
+                            <Text className="text-lg font-bold">{recipe.title}</Text>
+                            <Text className="text-sm">{recipe.description}</Text>
+                            {
+                                recipe?.author === userSession?._id &&
+                                <TouchableOpacity
+                                    className="bg-red-500 rounded-sm p-2 mt-1"
+                                    onPress={() => handleDeleteRecipe(recipe._id)}
+                                >
+                                    <Text className="text-white">Delete Recipe</Text>
+                                </TouchableOpacity>
+                            }
                         </TouchableOpacity>
                     </View>
                 ))}
